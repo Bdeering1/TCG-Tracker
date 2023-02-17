7b70e9b424bba4a5a828f0be294bee035ebba278
Bryn Deering <bdeering@uoguelph.ca>
2023-02-17 16:35:46 -0500

    Set up express and nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import express from 'express';
+
+const HTTP_OK = 200;
+const DEFAULT_PORT = 8080;
+
+const app = express();
+const port = process.env.PORT || DEFAULT_PORT;
+
+app.get('/', (_, res) => {
+    res.status(HTTP_OK).send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
